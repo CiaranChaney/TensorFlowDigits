fc8826f6eadb85d1da424fa6e6320614e8b47835
Ciaran <mrcchaney@gmail.com>
2024-10-20 15:42:05 +0100

    Completed recognition model

--- script.js
+++ script.js
@@ -29,10 +29,151 @@ async function showExamples(data) {
   }
 }
 
+function getModel() {
+    const model = tf.sequential();
+
+    const IMAGE_HEIGHT = 28;
+    const IMAGE_WIDTH = 28;
+    const IMAGE_CHANNELS = 1;
+
+    // In layer 1 of the convolutional neural network we have to specify input shape
+    // We also specify parameters for the convolution operation
+    model.add(tf.layers.conv2d({
+        inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+        kernelSize: 5,
+        filters: 8,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+    }));
+
+    // Max pooling layer downsamples using max values in a region instead of averaging
+    model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+    // Repeat with another conv2d + maxPooling
+    model.add(tf.layers.conv2d({
+        kernelSize: 5,
+        filters: 16,
+        strides: 1,
+        activation: 'relu',
+        kernelInitializer: 'varianceScaling'
+    }));
+
+    model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+    // Now flatten the output into a 1d vector for input to the last layer
+    model.add(tf.layers.flatten());
+
+    // Last layer is a dense layer with 10 outputs for 0-9
+    const NUM_OUTPUT_CLASSES = 10;
+    model.add(tf.layers.dense({
+        units: NUM_OUTPUT_CLASSES,
+        kernelInitializer: 'varianceScaling',
+        activation: 'softmax'
+    }));
+
+    // Then add an optimiser, loss function and accuracy metrics
+    // Compile and return the model
+    const optimiser = tf.train.adam();
+    model.compile({
+        optimizer: optimiser,
+        loss: 'categoricalCrossentropy',
+        metrics: ['accuracy']
+    });
+
+    return model;
+}
+
+async function train(model, data) {
+    const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
+    const container = {
+        name: 'Model Training', tab: 'Model', styles: { height: '1000px'}
+    };
+    const fitCallBacks = tfvis.show.fitCallbacks(container, metrics);
+
+    const BATCH_SIZE = 512;
+    const TRAIN_DATA_SIZE = 25000; // Can be upped to 55000
+    const TEST_DATA_SIZE = 2000; // Can be upped to 10000
+
+    const [trainXs, trainYs] = tf.tidy(() => {
+        const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+        return [
+            d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
+            d.labels
+        ];
+    });
+
+    const [testXs, testYs] = tf.tidy(() => {
+        const d = data.nextTestBatch(TEST_DATA_SIZE);
+        return [
+            d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
+            d.labels
+        ];
+    });
+
+    const history = await model.fit(trainXs, trainYs, {
+        batchSize: BATCH_SIZE,
+        validationData: [testXs, testYs],
+        epochs: 10,
+        shuffle: true,
+        callbacks: fitCallBacks
+    });
+
+    // Dispose of the large training and test tensors to free up memory
+    trainXs.dispose();
+    trainYs.dispose();
+    testXs.dispose();
+    testYs.dispose();
+
+    return history;
+    
+}
+
+const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+function doPrediction(model, data, testDataSize = 1000){
+    const IMAGE_HEIGHT = 28;
+    const IMAGE_WIDTH = 28;
+    const testData = data.nextTestBatch(testDataSize);
+    const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+    const labels = testData.labels.argMax(-1);
+    const preds = model.predict(testxs).argMax(-1);
+
+    testxs.dispose();
+    return[preds, labels];
+}
+
+async function showAccuracy(model, data){
+    const [preds, labels] = doPrediction(model, data);
+    const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+    const container = {name: 'Accuracy', tab: 'Evaluation'};
+    tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
+
+    labels.dispose();
+}
+
+async function showConfusion(model, data) {
+    const [preds, labels] = doPrediction(model, data);
+    const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+    const container = {name: 'Confusion Matrix', tab: 'Evaluation'};
+    tfvis.render.confusionMatrix(container, {values: confusionMatrix, tickLabels: classNames});
+
+    labels.dispose();
+    
+}
+
 async function run() {  
   const data = new MnistData();
   await data.load();
   await showExamples(data);
+
+  const model = getModel();
+  tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
+
+  await train(model, data);
+
+  await showAccuracy(model, data);
+  await showConfusion(model, data);
 }
 
 document.addEventListener('DOMContentLoaded', run);
